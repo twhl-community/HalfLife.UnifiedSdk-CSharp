5b1e0e9e1faf9998f9a18fe7d68483980e280d88
Sam V <sam.vanheer@outlook.com>
2022-03-08 20:04:47 +0100

    Fix Blue Shift BSP files being saved using the Blue Shift BSP format instead of the standard BSP format Resolves #1

--- src/HalfLife.UnifiedSdk.Utilities/Serialization/SledgeBSPFile/BSPSerializer.cs
+++ src/HalfLife.UnifiedSdk.Utilities/Serialization/SledgeBSPFile/BSPSerializer.cs
@@ -11,6 +11,12 @@ namespace HalfLife.UnifiedSdk.Utilities.Serialization.SledgeBSPFile
         public Map Deserialize(string fileName, Stream stream)
         {
             var bspFile = new BspFile(stream);
+
+            //Force this off so bsp files saved to disk can be loaded when used in normal maps.
+            //This means Blue Shift maps can't be saved back to the original game directory,
+            //but that's not something you should be doing with this tool.
+            bspFile.Options.UseBlueShiftFormat = false;
+
             return new BSPMap(fileName, bspFile);
         }
     }
