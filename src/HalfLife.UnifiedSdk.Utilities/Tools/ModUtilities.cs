388b6e1044cc26fc54aba59ad6bfea2b5bacf04d
Sam V <sam.vanheer@outlook.com>
2022-03-13 22:39:58 +0100

    Add helpers to get and format mod directory suffixes

--- src/HalfLife.UnifiedSdk.Utilities/Tools/ModUtilities.cs
+++ src/HalfLife.UnifiedSdk.Utilities/Tools/ModUtilities.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace HalfLife.UnifiedSdk.Utilities.Tools
 {
@@ -10,6 +11,66 @@ namespace HalfLife.UnifiedSdk.Utilities.Tools
     {
         /// <summary>The name of the liblist.gam file used to describe mod information.</summary>
         public const string LiblistFileName = "liblist.gam";
+
+        /// <summary>Types of content that can be enabled in Half-Life 1 mods.</summary>
+        public static class ContentTypes
+        {
+            /// <summary>The suffix used for high definition content directories.</summary>
+            public const string HighDefinition = "hd";
+
+            /// <summary>The suffix used for low violence content directories.</summary>
+            public const string LowViolence = "lv";
+
+            /// <summary>The suffix used for addon content directories.</summary>
+            public const string Addon = "addon";
+
+            /// <summary>The suffix used for downloads content directories.</summary>
+            public const string Downloads = "downloads";
+
+            /// <summary>Content types that are "public", meaning they should be distributed with mods.</summary>
+            /// <remarks>
+            /// Addon is meant for user-installed content separate from a game or mod.
+            /// Downloads is meant for files downloaded from servers.
+            /// </remarks>
+            public static IEnumerable<string> PublicTypes
+            {
+                get
+                {
+                    yield return HighDefinition;
+                    yield return LowViolence;
+                }
+            }
+
+            /// <summary>All content types.</summary>
+            public static IEnumerable<string> AllTypes
+            {
+                get
+                {
+                    yield return HighDefinition;
+                    yield return LowViolence;
+                    yield return Addon;
+                    yield return Downloads;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerable collection of all mod directory suffixes that are considered to be "public",
+        /// meaning they should be distributed with mods.
+        /// </summary>
+        public static IEnumerable<string> AllPublicModDirectorySuffixes => ContentTypes.PublicTypes.Concat(SteamUtilities.SteamLanguagesExceptEnglish);
+
+        /// <summary>Formats a mod directory that contains a suffix.</summary>
+        /// <seealso cref="ContentTypes"/>
+        /// <seealso cref="AllPublicModDirectorySuffixes"/>
+        /// <seealso cref="SteamUtilities.SteamLanguagesExceptEnglish"/>
+        public static string FormatModDirectory(string modDirectory, string suffix)
+        {
+            ArgumentNullException.ThrowIfNull(modDirectory);
+            ArgumentNullException.ThrowIfNull(suffix);
+
+            return $"{modDirectory}_{suffix}";
+        }
 
         /// <summary>
         /// Returns an enumerable collection of mod directory names in a specified path.
