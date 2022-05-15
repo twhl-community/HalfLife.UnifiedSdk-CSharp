08bf409d4ee28ef2adb144cb2bc8af1b65fe7e8b
Sam V <sam.vanheer@outlook.com>
2022-05-15 19:53:09 +0200

    Port asset synchronizer to Visual Studio project

---
+++ src/HalfLife.UnifiedSdk.AssetSynchronizer/ManifestFilter.cs
@@ -0,0 +1,31 @@
+﻿using HalfLife.UnifiedSdk.Utilities.Configuration;
+using Newtonsoft.Json;
+
+namespace HalfLife.UnifiedSdk.AssetSynchronizer
+{
+    internal class ManifestFilter
+    {
+        /// <summary>
+        /// Path relative to the root of the assets directory to start looking for files.
+        /// </summary>
+        [JsonConverter(typeof(PathConverter))]
+        public string Source { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Path relative to the root of the game directory to copy files to.
+        /// </summary>
+        [JsonConverter(typeof(PathConverter))]
+        public string Destination { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Pattern to filter files with. Supports wildcards.
+        /// </summary>
+        [JsonConverter(typeof(PathConverter))]
+        public string Pattern { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Whether to recurse into subdirectories. Default <see langword="true"/>.
+        /// </summary>
+        public bool Recursive { get; set; } = true;
+    }
+}
---
+++ src/HalfLife.UnifiedSdk.AssetSynchronizer/Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using RoboSharp;
+using System.Collections.Immutable;
+using System.CommandLine;
+using System.CommandLine.IO;
+using System.Text;
+
+namespace HalfLife.UnifiedSdk.AssetSynchronizer
+{
+    internal static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var assetsDirectoryOption = new Option<DirectoryInfo>("--assets-directory", description: "Path to the assets directory");
+            var gameDirectoryOption = new Option<DirectoryInfo>("--game-directory", description: "Path to the game directory");
+            var assetManifestOption = new Option<FileInfo>("--asset-manifest", description: "Path to the asset manifest file");
+
+            var rootCommand = new RootCommand("Half-Life Unified SDK asset synchronizer")
+{
+    assetsDirectoryOption,
+    gameDirectoryOption,
+    assetManifestOption,
+};
+
+            rootCommand.SetHandler((
+                DirectoryInfo assetsDirectory, DirectoryInfo gameDirectory,
+                FileInfo assetManifest,
+                IConsole console) =>
+            {
+                if (!gameDirectory.Exists)
+                {
+                    console.Error.WriteLine($"The given game directory \"{gameDirectory}\" does not exist");
+                    return;
+                }
+
+                //Needed because RoboSharp relies on a codepage not available in NET Core by default.
+                CodePagesEncodingProvider.Instance.GetEncoding(437);
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                var manifest = JsonConvert.DeserializeObject<List<ManifestFilter>>(File.ReadAllText(assetManifest.FullName)) ?? new();
+
+                var watchers = manifest.Select(filter =>
+                {
+                    var copy = new RoboCommand();
+
+                    copy.CopyOptions.Source = Path.Combine(assetsDirectory.FullName, filter.Source);
+                    copy.CopyOptions.Destination = Path.Combine(gameDirectory.FullName, filter.Destination);
+                    copy.CopyOptions.FileFilter = new[] { filter.Pattern };
+                    copy.CopyOptions.CopySubdirectories = filter.Recursive;
+                    copy.CopyOptions.Purge = true;
+                    copy.CopyOptions.MonitorSourceChangesLimit = 1;
+                    copy.CopyOptions.MonitorSourceTimeLimit = 1;
+
+                    copy.Start();
+
+                    return copy;
+                }).ToImmutableList();
+
+                console.Out.WriteLine("Watching for file changes");
+                console.Out.WriteLine("Press any key to stop...");
+                //Block until told to stop.
+                Console.ReadKey();
+                console.Out.Write("Stopping watchers...");
+
+                foreach (var watcher in watchers)
+                {
+                    watcher.Dispose();
+                }
+
+                console.Out.WriteLine("done");
+            }, assetsDirectoryOption, gameDirectoryOption, assetManifestOption);
+
+            return rootCommand.Invoke(args);
+        }
+    }
+}
