af8a7373277f6ec004362d454f4acf13f841c2a0
Sam V <sam.vanheer@outlook.com>
2022-05-15 19:33:10 +0200

    Replace hardcoded file lists with JSON file, hardcoded package name & game directory with parameters

---
+++ src/HalfLife.UnifiedSdk.Packager/PackageManifest.cs
@@ -0,0 +1,22 @@
+﻿using HalfLife.UnifiedSdk.Utilities.Configuration;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace HalfLife.UnifiedSdk.Packager
+{
+    internal class PackageManifest
+    {
+        /// <summary>
+        /// List of files and directories to package.
+        /// Filenames ending with ".install" will be renamed to remove this extension after being added to the archive.
+        /// </summary>
+        [JsonProperty(ItemConverterType = typeof(PathConverter))]
+        public List<string> Include { get; set; } = new();
+
+        /// <summary>
+        /// Files to exclude from the archive.
+        /// </summary>
+        [JsonProperty(ItemConverterType = typeof(PathConverter))]
+        public List<string> Exclude { get; set; } = new();
+    }
+}
---
+++ src/HalfLife.UnifiedSdk.Utilities/Configuration/PathConverter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace HalfLife.UnifiedSdk.Utilities.Configuration
+{
+    /// <summary>
+    /// Converts paths to and from platform-specific form.
+    /// </summary>
+    public sealed class PathConverter : JsonConverter<string>
+    {
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, string? value, JsonSerializer serializer)
+        {
+            var path = value?.Replace(Path.DirectorySeparatorChar, '/');
+            writer.WriteValue(path);
+        }
+
+        /// <inheritdoc/>
+        public override string? ReadJson(JsonReader reader, Type objectType, string? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return ((string?)reader.Value)?.Replace('/', Path.DirectorySeparatorChar);
+        }
+    }
+}
