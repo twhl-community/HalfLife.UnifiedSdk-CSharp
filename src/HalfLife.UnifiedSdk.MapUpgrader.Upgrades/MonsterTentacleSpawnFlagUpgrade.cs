f463c0fe30c518fb002a7f2fb3c7da2c8884de17
Sam V <sam.vanheer@outlook.com>
2022-05-19 10:40:57 +0200

    Add upgrade to convert monster_tentacle entities with spawnflag 64 set to use a different model malortie/custom-hl-viewmodels#45

---
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/MonsterTentacleSpawnFlagUpgrade.cs
@@ -0,0 +1,23 @@
+﻿using HalfLife.UnifiedSdk.Utilities.Entities;
+using HalfLife.UnifiedSdk.Utilities.Tools.UpgradeTool;
+
+namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades
+{
+    /// <summary>
+    /// Converts the Opposing Force <c>monster_tentacle</c> "Use Lower Model" spawnflag to instead set a custom model on the entity.
+    /// </summary>
+    internal sealed class MonsterTentacleSpawnFlagUpgrade : IMapUpgradeAction
+    {
+        private const int UseLowerModel = 1 << 6;
+
+        public void Apply(MapUpgradeContext context)
+        {
+            foreach (var tentacle in context.Map.Entities
+                .OfClass("monster_tentacle")
+                .Where(e => (e.GetSpawnFlags() & UseLowerModel) != 0))
+            {
+                tentacle.SetModel("models/tentacle2_lower.mdl");
+            }
+        }
+    }
+}
