d166516002da36b132346e26199c175881df5484
Sam V <sam.vanheer@outlook.com>
2022-07-13 20:21:54 +0200

    Allow user to choose which game to upgrade map for

--- src/HalfLife.UnifiedSdk.MapUpgrader/Program.cs
+++ src/HalfLife.UnifiedSdk.MapUpgrader/Program.cs
@@ -11,18 +11,54 @@ namespace HalfLife.UnifiedSdk.Installer
     {
         public static int Main(string[] args)
         {
+            var games = ValveGames.GoldSourceGames;
+
+            var defaultGame = ValveGames.HalfLife1;
+
+            var gameOption = new Option<string>("--game", description: "The name of a game's mod directory to apply upgrades for that game."
+                + $"If not specified, uses \"{defaultGame.ModDirectory}\"");
+
+            gameOption.AddCompletions(games.Select(g => g.ModDirectory).ToArray());
+
+            gameOption.AddValidator((result) =>
+            {
+                var game = result.GetValueForOption(gameOption)!;
+
+                if (!games.Any(g => g.ModDirectory == game))
+                {
+                    result.ErrorMessage = $"Invalid game \"{game}\" specified.";
+                }
+            });
+
             var mapsOption = new Option<IEnumerable<FileInfo>>("--maps", description: "List of maps to upgrade");
 
             var rootCommand = new RootCommand("Half-Life Unified SDK map upgrader")
             {
+                gameOption,
                 mapsOption
             };
 
-            rootCommand.SetHandler((IEnumerable<FileInfo> maps, IConsole console) =>
+            rootCommand.SetHandler((string? game, IEnumerable<FileInfo> maps, IConsole console) =>
             {
+                game ??= defaultGame.ModDirectory;
+
+                var gameInfo = games.SingleOrDefault(g => g.ModDirectory == game);
+
+                if (gameInfo is null)
+                {
+                    //Should never get here.
+                    return;
+                }
+
+                if (!maps.Any())
+                {
+                    console.WriteLine("No maps to upgrade");
+                    return;
+                }
+
                 var upgradeTool = MapUpgradeToolFactory.Create();
 
-                console.WriteLine($"Upgrading maps to version {upgradeTool.LatestVersion}");
+                console.WriteLine($"Upgrading maps for game {gameInfo.Name} ({gameInfo.ModDirectory}) to version {upgradeTool.LatestVersion}");
 
                 foreach (var map in maps)
                 {
@@ -32,14 +68,13 @@ namespace HalfLife.UnifiedSdk.Installer
 
                     console.Out.Write($"Upgrading \"{map.FullName}\" from version {currentVersion}");
 
-                    //TODO: let user specify which game the map is from.
-                    upgradeTool.Upgrade(new MapUpgradeCommand(mapData, ValveGames.HalfLife1));
+                    upgradeTool.Upgrade(new MapUpgradeCommand(mapData, gameInfo));
 
                     using var stream = File.Open(map.FullName, FileMode.Create, FileAccess.Write);
 
                     mapData.Serialize(stream);
                 }
-            }, mapsOption);
+            }, gameOption, mapsOption);
 
             return rootCommand.Invoke(args);
         }
