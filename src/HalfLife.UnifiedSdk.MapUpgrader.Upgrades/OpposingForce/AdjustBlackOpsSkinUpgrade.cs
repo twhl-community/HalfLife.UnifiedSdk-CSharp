362b73b2c2f5b1645689a755b76bbfaf9d6a33d9
Sam V <sam.vanheer@outlook.com>
2022-07-16 13:06:19 +0200

    Adjust Black Ops head and skin values malortie/custom-hl-viewmodels#36

---
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/OpposingForce/AdjustBlackOpsSkinUpgrade.cs
@@ -0,0 +1,37 @@
+﻿using HalfLife.UnifiedSdk.Utilities.Entities;
+using HalfLife.UnifiedSdk.Utilities.Tools.UpgradeTool;
+
+namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.OpposingForce
+{
+    /// <summary>
+    /// Adjust <c>monster_male_assassin</c> NPCs to use the correct head and skin value.
+    /// </summary>
+    internal sealed class AdjustBlackOpsSkinUpgrade : IMapUpgradeAction
+    {
+        public void Apply(MapUpgradeContext context)
+        {
+            foreach (var entity in context.Map.Entities.OfClass("monster_male_assassin"))
+            {
+                var head = entity.GetInteger("head");
+
+                int skin = 0;
+
+                switch (head)
+                {
+                    case 1:
+                        head = 0;
+                        skin = 1;
+                        break;
+
+                    case 2:
+                        head = 1;
+                        skin = 1;
+                        break;
+                }
+
+                entity.SetInteger("head", head);
+                entity.SetInteger("skin", skin);
+            }
+        }
+    }
+}
