6a4fd5cadf88818196122d89950db277500017b5
Sam V <sam.vanheer@outlook.com>
2022-10-10 18:57:45 +0200

    Fix Barney in ba_security2 greeting player as Freeman

--- src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/BlueShift/ChangeBlueShiftSentencesUpgrade.cs
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/BlueShift/ChangeBlueShiftSentencesUpgrade.cs
@@ -10,14 +10,19 @@ namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.BlueShift
     /// </summary>
     internal sealed class ChangeBlueShiftSentencesUpgrade : IMapUpgradeAction
     {
-        private const string MessageKey = "message";
-
         private static readonly ImmutableDictionary<string, string> SentenceMap = new Dictionary<string, string>
         {
             // The NA group is for No Access, EA is for Enable Access.
             // BS incorrectly adds an access granted sentence to the NA group.
             { "!NA1", "!EA0" },
+            // ba_security2 armory guard greets players as Freeman if using the original line.
+            { "!BA_HELLO1", "!BSBA_HELLO1" },
         }.ToImmutableDictionary(StringComparer.OrdinalIgnoreCase);
+
+        private static readonly ImmutableList<(string ClassName, string Key)> EntitiesToCheck = ImmutableList.Create(
+            ("ambient_generic", "message"),
+            ("scripted_sentence", "sentence")
+            );
 
         public void Apply(MapUpgradeContext context)
         {
@@ -26,11 +31,14 @@ namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.BlueShift
                 return;
             }
 
-            foreach (var entity in context.Map.Entities.OfClass("ambient_generic"))
+            foreach (var (className, key) in EntitiesToCheck)
             {
-                if (entity.TryGetValue(MessageKey, out var value) && SentenceMap.TryGetValue(value, out var replacement))
+                foreach (var entity in context.Map.Entities.OfClass(className))
                 {
-                    entity.SetString(MessageKey, replacement);
+                    if (entity.TryGetValue(key, out var value) && SentenceMap.TryGetValue(value, out var replacement))
+                    {
+                        entity.SetString(key, replacement);
+                    }
                 }
             }
         }
