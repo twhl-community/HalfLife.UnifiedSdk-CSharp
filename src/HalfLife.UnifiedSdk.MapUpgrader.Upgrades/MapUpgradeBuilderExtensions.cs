33f524553beb48e850c5dcc3f8fa7d4e5d8205c9
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2023-01-05 16:17:58 -0500

    Change Gina model in ba_security2 to use hologram pushcart model

--- src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/MapUpgradeBuilderExtensions.cs
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/MapUpgradeBuilderExtensions.cs
@@ -52,6 +52,7 @@ namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades
             builder.AddAction(new RemapRosenbergNoUseFlagUpgrade());
             builder.AddAction(new ChangeBlueShiftSentencesUpgrade());
             builder.AddAction(new BaXen3FixFlyBySoundsUpgrade());
+            builder.AddAction(new BaSecurity2ChangeHologramModelUpgrade());
             return builder;
         }
     }
