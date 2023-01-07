f20c6de965c6e899f120f356eee8bed70629aa6d
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2023-01-07 12:39:30 -0500

    Fix dead scientist head in ba_yard1.
    The original scientist head was using Rosenberg, and the same dead scientist from ba_yard4 was using Glasses head.

---
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/BlueShift/BaYard1FixDeadScientistModelUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using HalfLife.UnifiedSdk.Utilities.Entities;
+using HalfLife.UnifiedSdk.Utilities.Tools.UpgradeTool;
+
+namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.BlueShift
+{
+    /// <summary>
+    /// Changes incorrect dead scientist head (Rosenberg) in ba_yard1 to use the same as in ba_yard4 (Glasses).
+    /// </summary>
+    internal sealed class BaYard1FixDeadScientistModelUpgrade : MapSpecificUpgradeAction
+    {
+        public BaYard1FixDeadScientistModelUpgrade()
+            : base("ba_yard1")
+        {
+        }
+
+        protected override void ApplyCore(MapUpgradeContext context)
+        {
+            foreach (var entity in context.Map.Entities
+                .OfClass("monster_scientist_dead")
+                .Where(e => e.GetInteger("body") == 3))
+            {
+                entity.SetInteger("body", 0);
+            }
+        }
+    }
+}
