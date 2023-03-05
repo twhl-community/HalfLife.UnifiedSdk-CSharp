83a980cdb7386eead170a1d8425892546116063c
Sam V <sam.vanheer@outlook.com>
2023-03-05 22:01:11 +0100

    Implement Materials2Json

---
+++ src/HalfLife.UnifiedSdk.Formats/Materials/MaterialsConverter.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using Serilog;
+using System.Text.RegularExpressions;
+
+namespace HalfLife.UnifiedSdk.Formats.Materials
+{
+    /// <summary>
+    /// Converts original Half-Life <c>materials.txt</c> data to <c>materials.json</c>.
+    /// </summary>
+    public static class MaterialsConverter
+    {
+        private static readonly Regex LineRegex = new(@"^\s*([A-Z])?(\s+\w+)?\s*(\/\/.*)?$");
+
+        public static void Convert(Stream inputStream, Stream outputStream, ILogger logger)
+        {
+            using var reader = new StreamReader(inputStream);
+            using var writer = new JsonTextWriter(new StreamWriter(outputStream));
+
+            Dictionary<string, string> knownMaterials = new();
+
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 1;
+            writer.IndentChar = '\t';
+
+            writer.WriteStartObject();
+
+            int lineNumber = 0;
+
+            string? line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                ++lineNumber;
+
+                var match = LineRegex.Match(line);
+
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                // This can either be a comment on its own or a comment that follows a material entry.
+                // If it is a comment that followed an entry then it should be placed before the key in the JSON file.
+                if (match.Groups[3].Success)
+                {
+                    writer.WriteWhitespace(Environment.NewLine);
+                    writer.WriteWhitespace("\t");
+
+                    writer.WriteComment(match.Groups[3].Value[2..]);
+                }
+
+                if (match.Groups[1].Success && match.Groups[2].Success)
+                {
+                    var materialName = match.Groups[2].Value.Trim();
+                    var type = match.Groups[1].Value.ToUpperInvariant();
+
+                    if (knownMaterials.TryGetValue(materialName, out var existingType))
+                    {
+                        if (type == existingType)
+                        {
+                            logger.Warning("Ignoring duplicate material \"{Material}\"", materialName);
+                        }
+                        else
+                        {
+                            logger.Warning(
+                                "Ignoring duplicate material \"{Material}\" with differing types (existing: {Existing}, new: {New})",
+                                materialName, existingType, type);
+                        }
+                    }
+                    else
+                    {
+                        knownMaterials.Add(materialName, type);
+
+                        writer.WritePropertyName(materialName);
+
+                        writer.WriteStartObject();
+
+                        writer.WritePropertyName("Type");
+                        writer.WriteValue(type);
+
+                        writer.WriteEndObject();
+                    }                   
+                }
+                else if(match.Groups[1].Success || match.Groups[2].Success)
+                {
+                    throw new ConverterException("Invalid material entry")
+                    {
+                        LineNumber = lineNumber
+                    };
+                }
+            }
+
+            writer.WriteEndObject();
+
+            logger.Information("Wrote {Count} materials", knownMaterials.Count);
+        }
+    }
+}
---
+++ src/HalfLife.UnifiedSdk.Materials2Json/Program.cs
@@ -0,0 +1,49 @@
+﻿using HalfLife.UnifiedSdk.Formats.Materials;
+using HalfLife.UnifiedSdk.Utilities.Logging;
+using System.CommandLine;
+using System.CommandLine.Parsing;
+
+namespace HalfLife.UnifiedSdk.Materials2Json
+{
+    internal static class Program
+    {
+        const string TargetExtension = ".json";
+
+        public static async Task<int> Main(string[] args)
+        {
+            var inputFileName = new Argument<FileInfo>("filename", "materials to convert");
+
+            var outputFileName = new Option<FileInfo?>("--output-filename",
+                getDefaultValue: () => null,
+                "If provided, the name of the file to write the materials.json contents to");
+
+            var rootCommand = new RootCommand("Half-Life Unified SDK materials.txt to materials.json converter")
+            {
+                inputFileName,
+                outputFileName
+            };
+
+            rootCommand.SetHandler((fileName, outputFileName, logger) =>
+            {
+                if (fileName.Extension == TargetExtension)
+                {
+                    throw new ArgumentException(
+                        $"Input file \"{fileName.FullName}\" has the same extension as the target file type", nameof(fileName));
+                }
+
+                //Default to input with different extension.
+                outputFileName ??= new FileInfo(Path.ChangeExtension(fileName.FullName, TargetExtension));
+
+                logger.Information("Writing output to \"{FileName}\"", outputFileName.FullName);
+
+                using var inputStream = fileName.OpenRead();
+                using var outputStream = outputFileName.Open(FileMode.Create);
+
+                MaterialsConverter.Convert(inputStream, outputStream, logger);
+            },
+                inputFileName, outputFileName, LoggerBinder.Instance);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+    }
+}
