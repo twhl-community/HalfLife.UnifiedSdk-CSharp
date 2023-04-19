4c6acb7cf59db570ce6a4295c0f1a12a99846305
Sam V <sam.vanheer@outlook.com>
2023-04-19 21:32:07 +0200

    Don't modify worldspawn spawnflags (cannot be set through editor)

--- src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/OpposingForce/RemoveGameModeSettingsUpgrade.cs
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/OpposingForce/RemoveGameModeSettingsUpgrade.cs
@@ -1,17 +1,13 @@
-﻿using HalfLife.UnifiedSdk.Utilities.Entities;
-using HalfLife.UnifiedSdk.Utilities.Games;
+﻿using HalfLife.UnifiedSdk.Utilities.Games;
 using HalfLife.UnifiedSdk.Utilities.Tools.UpgradeTool;
 
 namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.OpposingForce
 {
     /// <summary>
-    /// Removes the CTF and Co-op game mode settings from Opposing Force maps.
+    /// Removes the CTF game mode settings from Opposing Force maps.
     /// </summary>
     internal sealed class RemoveGameModeSettingsUpgrade : GameSpecificMapUpgradeAction
     {
-        private const int CTFFlag = 1 << 3;
-        private const int CoopFlag = 1 << 4;
-
         public RemoveGameModeSettingsUpgrade()
             : base(ValveGames.OpposingForce)
         {
@@ -20,12 +16,6 @@ namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.OpposingForce
         protected override void ApplyCore(MapUpgradeContext context)
         {
             var worldspawn = context.Map.Entities.Worldspawn;
-
-            int spawnFlags = worldspawn.GetSpawnFlags();
-
-            spawnFlags &= ~(CTFFlag | CoopFlag);
-
-            worldspawn.SetSpawnFlags(spawnFlags);
 
             worldspawn.Remove("defaultctf");
         }
