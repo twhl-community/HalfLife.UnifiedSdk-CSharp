092b428792abf0f4cc4e5b26d4eaf71cfd6269ba
Sam V <sam.vanheer@outlook.com>
2023-05-29 12:51:37 +0200

    Fix c2a5 barrel not flying up if destroyed using radius damage attack

--- src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/HalfLife/C2a5FixBarrelPushTriggersUpgrade.cs
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/HalfLife/C2a5FixBarrelPushTriggersUpgrade.cs
@@ -16,12 +16,12 @@ namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.HalfLife
             OnceOnly = 1 << 0,
         }
 
-        private record BarrelSetup(string MultiManagerName, string PushName);
+        private record BarrelSetup(string MultiManagerName, string PushName, string BarrelName);
 
         private static readonly ImmutableArray<BarrelSetup> BarrelSetups = ImmutableArray.Create(
-            new BarrelSetup("can_expl2_mm", "can_expl2_push"),
-            new BarrelSetup("can_expl4_mm", "can_expl4_push"),
-            new BarrelSetup("can_expl5_mm", "can_expl5_push"));
+            new BarrelSetup("can_expl2_mm", "can_expl2_push", "can_expl2_shoot"),
+            new BarrelSetup("can_expl4_mm", "can_expl4_push", "can_expl4_shoot"),
+            new BarrelSetup("can_expl5_mm", "can_expl5_push", "can_expl5_shoot"));
 
         public C2a5FixBarrelPushTriggersUpgrade()
             : base("c2a5")
@@ -34,8 +34,9 @@ namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.HalfLife
             {
                 var multiManager = context.Map.Entities.Find(setup.MultiManagerName);
                 var push = context.Map.Entities.Find(setup.PushName);
+                var pushable = context.Map.Entities.Find(setup.BarrelName);
 
-                if (multiManager is null || push is null)
+                if (multiManager is null || push is null || pushable is null)
                 {
                     continue;
                 }
@@ -46,6 +47,22 @@ namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.HalfLife
                 var spawnFlags = push.GetSpawnFlags();
                 spawnFlags |= (int)TriggerPushSpawnFlag.OnceOnly;
                 push.SetSpawnFlags(spawnFlags);
+
+                // Nudge this pushable up by a unit.
+                // This prevents the pushable getting stuck in the ground.
+                // If the player uses a radius damage attack directed at the middle of the road
+                // (dead ahead exiting the tunnel)
+                // the specific entity setup used here won't allow the pushable to fly up otherwise
+                // because the engine thinks the pushable is somewhere else/outside the world.
+                // Specifically, when breakables are destroyed they clear the groundentity variable for entities
+                // (see CBreakable::Die)
+                // This particular entity setup happens to cause the variable to be cleared at just the right time
+                // for the pushable to touch the push trigger.
+                // This doesn't happen if a RadiusDamage attack is used due to slight timing changes.
+                // Only can_expl2_shoot has shown this behavior but just to be safe all of them are modified.
+                var origin = pushable.GetOrigin();
+                ++origin.Z;
+                pushable.SetOrigin(origin);
             }
         }
     }
