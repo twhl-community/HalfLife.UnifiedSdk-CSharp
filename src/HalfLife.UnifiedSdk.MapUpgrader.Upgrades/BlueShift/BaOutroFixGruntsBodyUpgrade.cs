163f2925a94b65bd511019ed853bd0bb562ff804
Sam V <sam.vanheer@outlook.com>
2023-08-31 11:51:28 +0200

    Add missing documentation for upgrade

--- src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/BlueShift/BaOutroFixGruntsBodyUpgrade.cs
+++ src/HalfLife.UnifiedSdk.MapUpgrader.Upgrades/BlueShift/BaOutroFixGruntsBodyUpgrade.cs
@@ -3,6 +3,10 @@ using HalfLife.UnifiedSdk.Utilities.Tools.UpgradeTool;
 
 namespace HalfLife.UnifiedSdk.MapUpgrader.Upgrades.BlueShift
 {
+    /// <summary>
+    /// Adjusts the body values of the Human Grunts in <c>ba_outro</c> to match the Half-Life script
+    /// and to avoid clipping weapons into Freeman.
+    /// </summary>
     internal sealed class BaOutroFixGruntsBodyUpgrade : MapSpecificUpgrade
     {
         public BaOutroFixGruntsBodyUpgrade()
